2de7575d18724898ad5e476b0da032c3806602f7
gretaivan <greta.ivan91@gmail.com>
2021-05-11 17:02:36 +0100

    sets up the db with schema

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, g
 from flask_cors import CORS
 from werkzeug import exceptions
 import sqlite3
+
 
 app = Flask(__name__)
 CORS(app)
@@ -13,21 +14,37 @@ def homepage():
     init_db()
     return "Welcome!"
 
-
+# DATABASE setup
 def get_db():
   db = getattr(g, '_database', None)
   if db is None:
     db = g._database = sqlite3.connect(DATABASE)
   return db
 
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 def init_db():
   with app.app_context():
     db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
+    with app.open_resource('database/schema.sql', mode='r') as f:
       db.cursor().executescript(f.read())
     db.commit()
 
+def query_db(query, args=(), one=False):
+  cur = get_db().execute(query, args)
+  get_db().commit()
+  rv = cur.fetchall()
+  cur.close()
+  return (rv[0] if rv else None) if one else rv
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
