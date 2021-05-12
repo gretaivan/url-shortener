6d8f7a372b4798510d22beea682ad7c14e2c36a8
gretaivan <greta.ivan91@gmail.com>
2021-05-12 11:57:18 +0100

    completes the separation of alias and all aliases route with templates and dynamic data

--- app.py
+++ app.py
@@ -9,15 +9,19 @@ app = Flask(__name__)
 CORS(app)
 
 DATABASE = 'database/database.db'
+URL = 'shoter.io'
 
 @app.route("/", methods=['GET','POST'])
 def homepage():
   if request.method == 'GET': 
-    return render_template('home.html')
+    return render_template('home.html', title="Save URL")
   elif request.method == 'POST':
     response, code = urls.create(request)
     if code == 201:
-      return render_template('home.html')
+      content = f'A new URL:  {request.form["url"]}, has been saved as '
+      link = f'http://127.0.0.1:5000/aliases/{response[0][1]}'
+      link_name =f'{URL}/{response[0][1]}'
+      return render_template('home.html', title="SAVED", content=content, link=link, linkName=link_name)
     else: 
       return redirect(url_for('aliases', alias=response))
 
@@ -31,7 +35,7 @@ def aliases(alias=None):
       resp, code = fns[request.method](request)
       return render_template('all_urls.html', content=resp), code
     else:
-      return render_template('alias.html', alias=alias)
+      return render_template('alias.html', alias=alias, title="")
   
   
 
